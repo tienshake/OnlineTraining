08ff73e1b1ef988fadb2b14abba9378bd4dbfecb
Truong Nguyen <nttruong.it2001@gmail.com>
2023-03-11 16:56:21 +0700

    update UI admin

---
+++ front-end/src/admin/componentsAdmin/EvaluateStar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+
+export default function EvaluateStar() {
+    return (
+        <div>
+            <ul className='group_evaluate_star'>
+                <li><AiFillStar /></li>
+                <li><AiFillStar /></li>
+                <li><AiFillStar /></li>
+                <li><AiFillStar /></li>
+                <li><AiFillStar /></li>
+            </ul>
+        </div>
+    )
+}
