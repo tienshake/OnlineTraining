6c18bcd7af277fd251809c3055e63fe28746bc67
tienshake <tientrancntt@gmail.com>
2023-03-16 21:20:14 +0700

    api edit user and detail

--- front-end/src/admin/store/features/userTeacher/userTeacherSlice.tsx
+++ front-end/src/admin/store/features/userTeacher/userTeacherSlice.tsx
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import type { PayloadAction, AnyAction } from "@reduxjs/toolkit";
 import axios from "axios";
+import userServices from "../../../../services/user";
 
 interface userTeacherState {
   dataUserTeacher: any;
@@ -21,20 +22,32 @@ const initialState: userTeacherState = {
 export const getDataUser: any = createAsyncThunk(
   "userTeacher/getDataUser",
   async (params, { rejectWithValue }) => {
-    try {
-      const { data } = await axios({
-        method: "get",
-        url: "http://localhost:8080/user/get?id=ALL",
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwicm9sZV9pZCI6MSwiZW1haWwiOiJ0aWVubkBnbWFpbC5jb20iLCJpYXQiOjE2Nzg5MDkxMjEsImV4cCI6MTY4MTUwMTEyMX0.P1WCkrn_4N0pr99_02crlcXmG7SQQnQgseY5F9z1TCY",
-          "Content-Type": "application/json",
-        },
-      });
+    // try {
+    //   const { data } = await axios({
+    //     method: "get",
+    //     url: "http://localhost:8080/user/get?id=ALL",
+    //     headers: {
+    //       Authorization:
+    //         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwicm9sZV9pZCI6MSwiZW1haWwiOiJ0aWVubkBnbWFpbC5jb20iLCJpYXQiOjE2Nzg5MDkxMjEsImV4cCI6MTY4MTUwMTEyMX0.P1WCkrn_4N0pr99_02crlcXmG7SQQnQgseY5F9z1TCY",
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
 
+    //   return data;
+    // } catch (err: any) {
+    //   return rejectWithValue(err.response.data);
+    // }
+    // const data: any = await userServices.getUserApi("ALL");
+    // console.log("data", data.data);
+    // if (data.response.status === 500) {
+    //   return rejectWithValue(data.response.data);
+    // }
+    // return data.data;
+    try {
+      const { data } = await userServices.getUserApi("ALL");
       return data;
-    } catch (err: any) {
-      return rejectWithValue(err.response.data);
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
     }
   }
 );
--- front-end/src/services/user.ts
+++ front-end/src/services/user.ts
@@ -2,14 +2,8 @@ import axiosClient from "../api/axiosClient";
 
 const userServices = {
   getUserApi: async (id: string) => {
-    try {
-      const response = await axiosClient.get(`user/get?id=${id}`);
-      console.log(response);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
+    const response = await axiosClient.get(`user/get?id=${id}`);
+    return response;
   },
 };
 
