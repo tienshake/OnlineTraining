f72096bdfc2c1f6e54d8870d2c50b73505f00975
Truong Nguyen <nttruong.it2001@gmail.com>
2023-03-17 08:12:43 +0700

    validate form UI  add user

--- front-end/src/admin/componentsAdmin/ShowModalForm.tsx
+++ front-end/src/admin/componentsAdmin/ShowModalForm.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
@@ -12,7 +11,10 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import './ComponentsAdmin.css'
+import { useState } from 'react';
+import { TypeObjectInput, TypeError, ErrorSubmit } from '../../types/index';
+import './ComponentsAdmin.css';
+
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -23,23 +25,132 @@ const style = {
 };
 
 export default function ShowModalForm() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event: any) => {
-    setAge(event.target.value);
-  };
-
-  const [showPassword, setShowPassword] = React.useState(false);
-
+  /* Show pass */
+  const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
+  /* mouseDown */
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
+
+
+  /* VALIDATE FORM AND SUBMIT */
+  const [inputs, setInputs] = useState<TypeObjectInput>({});
+  const [errors, setErrors] = useState<TypeError>({});
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nameInput = e.target.name;
+    let valueInput = e.target.value;
+
+    setInputs(state => ({ ...state, [nameInput]: valueInput }));
+  };
+
+  const options = [
+    { value: "", label: 'Role' },
+    { value: 1, label: 'Teacher' },
+    { value: 2, label: 'Student' },
+    { value: 3, label: 'Admin' },
+  ];
+  const [selectedOption, setSelectedOption] = useState(options[0].value);
+
+  /* select change */
+  const handleSelectChange = (e: any) => {
+    const nameInput = e.target.name;
+    let valueInput = e.target.value;
+
+    const optionValues = options.map((option) => option.value);
+    if (optionValues.includes(valueInput)) {
+      setSelectedOption(valueInput);
+    } else {
+      console.error(`Invalid valueInput ${valueInput} selected`);
+    }
+
+    setInputs(state => ({ ...state, [nameInput]: valueInput })) // 
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    let errorSubmit: ErrorSubmit = {
+
+    };
+
+    let check = false;
+
+    /* validate name */
+    if (inputs.name === undefined || inputs.name === '') {
+      errorSubmit.name = "Bạn vui lòng nhập name của mình !";
+      setErrors(errorSubmit);
+      check = false;
+    } else {
+      setErrors(errorSubmit);
+      check = true;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const checkEmail = emailRegex.test(`${inputs.email}`);
+    console.log(checkEmail, "ok")
+
+    /* validate email */
+    if (inputs.email === undefined || inputs.email === '') {
+      errorSubmit.email = "Bạn vui lòng nhập email của mình !";
+      setErrors(errorSubmit);
+      check = false;
+    } else if (!checkEmail) {
+      errorSubmit.email = "Email không đúng định dạng !";
+      setErrors(errorSubmit);
+      check = false;
+    } else {
+      setErrors(errorSubmit);
+      check = true;
+    }
+
+    /* validate password */
+    if (inputs.password === undefined || inputs.password === '') {
+      errorSubmit.password = "Bạn vui lòng nhập mật khẩu!";
+      setErrors(errorSubmit);
+      check = false;
+    } else {
+      setErrors(errorSubmit);
+      check = true;
+    }
+
+    /* validate role */
+    if (inputs.role === undefined || inputs.role === "") {
+      errorSubmit.role = "Bạn vui lòng nhập role!";
+      setErrors(errorSubmit);
+      check = false;
+    } else {
+      setErrors(errorSubmit);
+      check = true;
+    }
+
+    /* validate comfirm password */
+    if (inputs.confirmPass === undefined || inputs.confirmPass === '') {
+      errorSubmit.confirmPass = "Vui lòng nhập lại mật khẩu !";
+      setErrors(errorSubmit);
+      check = false;
+    } else if (inputs.confirmPass !== inputs.password) {
+      alert("Mật khẩu không khớp nhau!");
+      check = false;
+    } else {
+      setErrors(errorSubmit);
+      check = true;
+    }
+
+    /* submit  */
+    if (check) {
+      alert('đăng nhập Ok!')
+    } else {
+      alert('đăng nhập thất bại !')
+    }
+  }
+
+
 
 
   return (
@@ -57,26 +168,34 @@ export default function ShowModalForm() {
             Nhập Thông tin User
           </Typography>
 
-          <form style={{ display: 'flex', flexDirection: 'column' }}>
+          <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
             <FormControl fullWidth sx={{ m: 1 }} variant="standard">
               <InputLabel htmlFor="">Name</InputLabel>
               <Input
-              // id=""
+                // id=""
+                name="name"
+                onChange={handleInputChange}
               // startAdornment={<InputAdornment position="start">$</InputAdornment>}
               />
+              {errors.name === '' || errors.name === undefined ? null : <p style={{ color: "#D93025", textAlign: 'start', marginTop: '5px', fontSize: '14px' }}>{errors.name}</p>}
             </FormControl>
 
             <FormControl fullWidth sx={{ m: 1 }} variant="standard">
               <InputLabel htmlFor="">Email</InputLabel>
               <Input
-              // id=""
+                // id=""
+                name="email"
+                onChange={handleInputChange}
               />
+              {errors.email === '' || errors.email === undefined ? null : <p style={{ color: "#D93025", textAlign: 'start', marginTop: '5px', fontSize: '14px' }}>{errors.email}</p>}
             </FormControl>
 
             <FormControl sx={{ m: 1, width: '100%' }} variant="standard">
               <InputLabel htmlFor="">Password</InputLabel>
               <Input
                 // id=""
+                name="password"
+                onChange={handleInputChange}
                 type={showPassword ? 'text' : 'password'}
                 endAdornment={
                   <InputAdornment position="end">
@@ -90,11 +209,14 @@ export default function ShowModalForm() {
                   </InputAdornment>
                 }
               />
+              {errors.password === '' || errors.password === undefined ? null : <p style={{ color: "#D93025", textAlign: 'start', marginTop: '5px', fontSize: '14px' }}>{errors.password}</p>}
             </FormControl>
 
             <FormControl sx={{ m: 1, width: '100%', mb: 5 }} variant="standard">
               <InputLabel htmlFor="">Confirm Password</InputLabel>
               <Input
+                name="confirmPass"
+                onChange={handleInputChange}
                 // id=""
                 type={showPassword ? 'text' : 'password'}
                 endAdornment={
@@ -109,27 +231,31 @@ export default function ShowModalForm() {
                   </InputAdornment>
                 }
               />
+              {errors.confirmPass === '' || errors.confirmPass === undefined ? null : <p style={{ color: "#D93025", textAlign: 'start', marginTop: '5px', fontSize: '14px' }}>{errors.confirmPass}</p>}
             </FormControl>
 
             <FormControl sx={{ m: 1, width: '100%', mt: 0 }} size="small">
-              <InputLabel id="demo-select-small">Age</InputLabel>
+              <InputLabel id="demo-select-small">Role</InputLabel>
               <Select
                 labelId="demo-select-small"
                 id="demo-select-small"
-                value={age}
-                label="Age"
-                onChange={handleChange}
+                value={selectedOption}
+                label="role"
+                name="role"
+                onChange={handleSelectChange}
               >
-                <MenuItem value="">
-                  <em>Role</em>
-                </MenuItem>
-                <MenuItem value={10}>teacher</MenuItem>
-                <MenuItem value={20}>student</MenuItem>
+                {options.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </Select>
+              {errors.role === null || errors.role === undefined ? null : <p style={{ color: "#D93025", textAlign: 'start', marginTop: '5px', fontSize: '14px' }}>{errors.role}</p>}
             </FormControl>
 
+
             <div className='GroupBtn_Save-Cancel' style={{ width: '100%', display: 'flex', gap: '5px', marginTop: '20px' }}>
-              <Button variant="contained">Submit</Button>
+              <Button type='submit' variant="contained">Save</Button>
               <Button onClick={handleClose} variant="contained">Cancel</Button>
             </div>
           </form>
