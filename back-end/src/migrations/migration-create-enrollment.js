a403c63d1786902770857b72e8d0bd2f1c338179
VinOften99 <vin.oit99@gmail.com>
2023-03-18 12:18:03 +0700

    add

---
+++ back-end/src/migrations/migration-create-enrollment.js
@@ -0,0 +1,40 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Enrollments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      course_id: {
+        type: Sequelize.INTEGER,
+      },      
+      status: {
+        type: Sequelize.STRING,
+      },
+      payment_id: {
+        type: Sequelize.INTEGER,
+      },
+      enrollment_date: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Enrollments");
+  },
+};
---
+++ back-end/src/migrations/migration-create-nofitication.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Nofitications", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      course_id: {
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      nofitication_title: {
+        type: Sequelize.STRING,
+      },
+      nofitication_content: {
+        type: Sequelize.STRING,
+      },
+      nofitication_type: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Nofitications");
+  },
+};
