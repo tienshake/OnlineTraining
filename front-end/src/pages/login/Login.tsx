78262745f07b9758c31fc8dc46b4111255c4af85
ungnhohoangviet12 <ungnhohoangviet1@gmail.com>
2023-03-21 16:42:41 +0700

    login

--- front-end/src/pages/login/Login.tsx
+++ front-end/src/pages/login/Login.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import userServices from "../../services/user";
+import style from "./Login.module.scss"
+import logologin from "../../assets/images/login-img.png"
+import dream from "../../assets/images/dream.png"
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -32,12 +36,35 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login Form</h1>
-      <input type="email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={() => login(email, password)}>Login</button>
-      <button onClick={() => getUser("ALL")}>Get user</button>
+    <div className={style.body}>
+      <div className={style.left}>
+        <img src={logologin} alt="" />
+        <h2>Welcome to <br />
+        DreamsLMS Courses
+        </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+      </div>
+      <div className={style.right}>
+        <div className={style.dream}>
+          <img src={dream} alt="" />
+          <a href="">back to home</a>
+        </div>
+        <h2>Sign into Your Account</h2>
+        <form action="">
+          <label htmlFor="">Email</label>
+          <input type="text" placeholder="Enter your email address" />
+          <label htmlFor="">Password</label>
+          <input type="text" placeholder="Enter you password"/>
+          <label htmlFor="">Forgot Password ?</label>
+          <input type="text" />
+          <a href="Forgot Password?"></a>
+          <div>
+          <input type="checkbox" />
+          <label htmlFor="">Remember</label>
+          <button>Sign in</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
